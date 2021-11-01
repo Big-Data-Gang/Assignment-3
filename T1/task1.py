15fd1fa7ee1a3826cfc3945b5ae4698957f3719d
ArvindAROO <i.arvindkrishna@gmail.com>
2021-11-01 23:07:23 +0530

    T1 Done except for output formatting

--- T1/task1.py
+++ T1/task1.py
@@ -1,24 +1,39 @@
 #!/usr/bin/env python3
+from pyspark.sql import SparkSession
+from pyspark.sql import functions as F
+import sys
 
 # find all rows with a particular value in a column greater than the average in a csv file using pyspark
 def findRowsSpark(country, path):
-    from pyspark.sql import SparkSession
-    from pyspark.sql import functions as F
+
     spark = SparkSession.builder.appName("findRows").getOrCreate()
     df = spark.read.csv(path, header=True, inferSchema=True)
     df.show()
-    # find average of column for all groups of column 2
-    groupedByCity = df.groupBy("City").agg(F.mean('AverageTemperature')).show()
+    df = df.filter(df.Country == country)
+    # find average of column for all groups of column City
+    groupedByCity = df.groupBy("City").agg(F.mean('AverageTemperature'))
+    
     # join groupedByCity and df over City column
-    joined = df.join(groupedByCity, df.City == groupedByCity.City and df.Country == country)
+    joined = df.join(groupedByCity, on = ["City"], how='inner')
+    print("joined table")
+    joined.show()
+    
     # count rows where AverageTemperature is greater than the average
-    df.filter(df.AverageTemperature > joined.avg(joined.AverageTemperature)).show()
-    joined.filter(joined.AverageTemperature > joined.avg).show()    
+    print("Matched values for given criteria")
+    joined = joined.filter(joined.AverageTemperature > joined["avg(AverageTemperature)"])
+    joined.show()
+
+    #count the number of occurances of a particular city
+    final = joined.groupBy("City").count()
+    print("Only whatever is after this is the output>>>>")
+    final.show()
     spark.stop()
 
 if __name__ == '__main__':
-    import sys
+    # to test, execute the following command
+    # $SPARK_HOME/bin/spark-submit task1.py India city_sample.csv > output.txt 
+
     if len(sys.argv) != 3:
-        print("Usage: task1.py <country> <path>", file=sys.stderr)
+        raise Exception("Re-run the script in this format: task1.py <country> <path>")
         sys.exit(-1)
     findRowsSpark(sys.argv[1], sys.argv[2])
